0b89355a2c04f3c1438a0e1a67e8b486cab22e5c
Swayam Patil <patilswayam96@gmail.com>
2025-06-11 09:30:17 +0530

    bug fixes

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -182,7 +182,7 @@ SIMPLE_JWT = {
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
-    "https://fixmyresume.vercel.app",
+    "https://fix-my-resume.vercel.app",
 ]
 
 CORS_ALLOW_CREDENTIALS = True
